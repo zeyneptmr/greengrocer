b5763027e9fa1a91ada7d6dc21201339cf3ee71e
Selin <budak.selin3@gmail.com>
2025-02-20 17:55:21 +0300

    created navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/Navbar.css'
+import Logo from '../logo.svg';
+
+function Navbar() {
+  return (
+    <header className='header'>
+
+      <div className='logo-container'>
+        <img src={Logo} alt="Logo" className="logo" />
+        <a href='/' className='logo-name'>Logo</a>
+      </div>
+      
+      <nav className='navbaritems'>
+        <a href='/'>Home</a>
+        <a href='/'>About</a>
+        <a href='/'>Services</a>
+        <a href='/'>Contact</a>
+      </nav>
+
+      <div className='icons'>
+
+
+      </div>
+  
+    </header>
+  );
+}
+
+export default Navbar;
