9407377f62de8d0bb124aed0b80097a8620957df
Selin <budak.selin3@gmail.com>
2025-02-26 19:08:44 +0300

    tailwind is used for footer and navigations are added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Footer.css'
+import '../styles/Footer.css';
 import VegetableIcon from '../assets/vegetables.svg';
 import LocationIcon from '../assets/location.svg';
 import PhoneIcon from '../assets/phone.svg';
@@ -9,93 +9,77 @@ import FacebookIcon from '../assets/facebook.svg';
 import InstagramIcon from '../assets/instagram.svg';
 import YoutubeIcon from '../assets/youtube.svg';
 
-
 function Footer() {
   return (
-    <div className='main-footer'>
-        <div className='container'>
-        
-            <div className='row'>
-                
-                <div className='column'>
-                    <h4>CONTACT</h4>
-                    <br></br>
-                    
-                    <div className='locationcontainer'>
-                        <img src={LocationIcon} alt="LocationIcon" className="footericons" />
-                        <p>Cibali Mah. Kadir Has Cad. 34083
-                        Fatih-İstanbul</p>
-                    </div>
-
-                    <br></br>
-
-                    <div className='phonecontainer'>
-                        <img src={PhoneIcon} alt="PhoneIcon" className="footericons" />
-                        <p>(0212) 533 65 32</p>
-                    </div>
-                    
-                    <br></br>
-
-                    <div className='mailcontainer'>
-                        <img src={MailIcon} alt="MailIcon" className="footericons" />
-                        <p>taptaze@stu.khas.edu.tr</p>
-                        
-                    </div>    
-                       
-
-                </div>
-
-                <div className='vegetablecolumn'>
-                    <img src={VegetableIcon} alt="VegetableIcon" className="vegetableicon" />
-                </div>
-            
-                <div className='column'>
-                    <h4>COMPANY</h4>
-                    <br></br>
-                    
-                    <div className='aboutcontainer'>
-                    <img src={MegaphoneIcon} alt="MegaphoneIcon" className="footericons" />
-                        <a href='/'>About Us</a>
-                        
-                    </div> 
-
-                    <div className='socialmediacontainer'>
-                        <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
-                            <img src={FacebookIcon} alt="FacebookIcon" className="socialmediaicons" />
-                        </a>
-
-                        <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
-                        <img src={InstagramIcon} alt="InstagramIcon" className="socialmediaicons" />
-                        </a>
-
-                        <a href='https://www.youtube.com/' target='_blank' rel='noopener noreferrer'>
-                        <img src={YoutubeIcon} alt="YoutubeIcon" className="socialmediaicons" />
-                        </a>
-
-                    
-                    </div> 
+    <div className="bg-green-600 text-white py-8">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+          
+          {/* Contact */}
+          <div className="flex-1 min-w-[200px] ml-[-30px]">
+            <h4 className="text-lg font-bold">CONTACT</h4>
+            <div className="mt-4 space-y-2">
+              <div className="flex items-center">
+                <img src={LocationIcon} alt="Location" className="w-5 mr-2" />
+                <a 
+                  href="https://maps.google.com/?q=Cibali+Mah.+Kadir+Has+Cad.+34083+Fatih-İstanbul" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  className="hover:text-yellow-400 transition"
+                >
+                  Cibali Mah. Kadir Has Cad. 34083 Fatih-İstanbul
+                </a>
+              </div>
+              <div className="flex items-center">
+                <img src={PhoneIcon} alt="Phone" className="w-5 mr-2" />
+                <span>(0212) 533 65 32</span> 
+              </div>
+              <div className="flex items-center">
+                <img src={MailIcon} alt="Mail" className="w-5 mr-2" />
+                <a href="mailto:taptaze@stu.khas.edu.tr" className="text-white hover:text-yellow-400 transition">
+                taptaze@stu.khas.edu.tr
+                </a>
 
 
-                </div>
-
+              </div>
             </div>
-            <hr></hr>
-            <div className='row'>
-                <p className='copyright'>
-                    &copy; {new Date().getFullYear()} All Rights Reserved
+          </div>
 
-                </p>
+          {/* Vegetable Section */}
+          <div className="flex-1 flex justify-center">
+            <img src={VegetableIcon} alt="Vegetable Icon" className="w-24" />
+          </div>
 
+          {/* Company Section */}
+          <div className="flex-1 text-center">
+            <h4 className="text-lg font-bold">COMPANY</h4>
+            <div className="mt-4">
+              <a 
+                href="/" 
+                className="inline-flex items-center space-x-2 text-white hover:text-yellow-400 transition"
+              >
+                <img src={MegaphoneIcon} alt="Megaphone Icon" className="w-5" />
+                <span>About Us</span>
+              </a>
             </div>
-
-
-
+            <div className="mt-6 flex justify-center space-x-6">
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-150">
+                <img src={FacebookIcon} alt="Facebook Icon" className="w-10" />
+              </a>
+              <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-150">
+                <img src={InstagramIcon} alt="Instagram Icon" className="w-10" />
+              </a>
+              <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-150">
+                <img src={YoutubeIcon} alt="Youtube Icon" className="w-10" />
+              </a>
+            </div>
+          </div>
         </div>
 
-
-        
-      
-  
+        {/* Footer Bottom Section */}
+        <hr className="border-white my-6 w-full mx-auto" />
+        <p className="text-center text-sm">&copy; {new Date().getFullYear()} All Rights Reserved</p>
+      </div>
     </div>
   );
 }
