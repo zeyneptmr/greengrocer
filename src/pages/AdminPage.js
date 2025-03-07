4048ef60e2e9b660136f02ecf603fa193af759c2
Selin <budak.selin3@gmail.com>
2025-03-07 18:03:44 +0300

    card content is changed in AdminPage

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -27,11 +27,11 @@ const AdminPage = () => {
 
                         <div className="bg-white shadow-md rounded-lg p-6">
                             <h3 className="text-2xl font-bold text-green-700">Total Sales</h3>
-                            <p className="text-2xl text-gray-500 font-medium">$45,300</p>
+                            <p className="text-2xl text-gray-500 font-medium">45,300 TL</p>
                         </div>
 
                         <div className="bg-white shadow-md rounded-lg p-6">
-                            <h3 className="text-2xl font-bold text-green-700">New Orders</h3>
+                            <h3 className="text-2xl font-bold text-green-700">Total Number of Products</h3>
                             <p className="text-2xl text-gray-500 font-medium">120</p>
                         </div>
                     </div>
