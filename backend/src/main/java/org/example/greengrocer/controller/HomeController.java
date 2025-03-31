cd9819cb2a56aab1d7e71ee3315718b0f5e3293e
Selin <budak.selin3@gmail.com>
2025-04-01 00:59:51 +0300

    Latest versions of some backend pages

--- backend/src/main/java/org/example/greengrocer/controller/HomeController.java
+++ backend/src/main/java/org/example/greengrocer/controller/HomeController.java
@@ -1,3 +1,5 @@
+package org.example.greengrocer.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
