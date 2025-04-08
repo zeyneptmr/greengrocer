60dd18c33c8ac044de0c6d5015077c26ffa63fe0
zeynepkrtls <zeyzeykurtulus@gmail.com>
2025-04-09 01:48:19 +0300

    sidebar responsive

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,6 +15,7 @@ const Sidebar = () => {
     const navigate = useNavigate();
     const [role, setRole] = useState("");
     const { setIsLoggedIn } = useCart();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
     useEffect(() => {
         // Kullanıcı bilgilerini doğrulamak için /me endpoint'ini çağır
@@ -59,90 +60,115 @@ const Sidebar = () => {
     const dashboardLink = role === "MANAGER" ? "/manager" : role === "ADMIN" ? "/admin" : "/user/home";
 
     return (
-        <aside className="gap-4 w-64 bg-green-600 text-white flex flex-col p-4 h-screen">
-            <h2 className="text-2xl font-bold mb-6 text-center">{sidebarTitle}</h2>
-            <nav className="flex-1">
-                <ul className="space-y-4">
-                    <li>
-                        <Link to={dashboardLink} className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                            <Home size={25} />
-                            <span>Dashboard</span>
-                        </Link>
-                    </li>
-
-                    {role === "MANAGER" ?  (
-                        <li>
-                            <Link to="/manager/inventory" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                <Package size={25} />
-                                <span>Inventory</span>
-                            </Link>
-                        </li>
-                    ) : (
-                        <li>
-                            <Link to="/admin/displayproducts" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                <NotebookText size={25} />
-                                <span>Display Products</span>
-
-                            </Link>
-                        </li>
-
-                    )}
-
-                    {role === "MANAGER" ?  (
-                        <>
-                            <li>
-                                <Link to="/manager/discounts"
-                                      className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                    <MdPercent size={25}/>
-                                    <span>Discounts</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/manager/customer-order"
-                                      className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                    <ClipboardList size={25}/>
-                                    <span>Customer Orders</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/manager/customer-feedback"
-                                      className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                    <MessageCircle size={25}/>
-                                    <span>Customer Feedback</span>
-                                </Link>
-                            </li>
-
-                        </>
-                    ) : (
-                        role === "ADMIN" && (
-                            <>
-                                <li>
-                                    <Link to="/admin/addproducts"
-                                          className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                        <Plus size={20} />
-                                        <span>Add Products</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/admin/updateproducts" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
-                                        <Pen size={20} />
-                                        <span>Update Products</span>
-                                    </Link>
-                                </li>
-                            </>
-                        )
-                    )}
-                </ul>
-            </nav>
-            <div className="mt-auto mb-20 flex justify-center">
+        <>
+            {/* Hamburger Menu Icon (Top-left corner) */}
+            <div className="fixed top-4 left-4 z-50">
                 <button
-                    onClick={handleLogout}
-                    className="w-10 h-10 bg-red-600 rounded-full flex items-center justify-center hover:bg-red-700 transition-transform duration-200 hover:scale-125">
-                    <LogOut size={24} className="text-white" />
+                    onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                    className="text-orange-500 hover:text-orange-700 focus:outline-none"
+                >
+                    <svg
+                        className="w-8 h-8"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
                 </button>
             </div>
-        </aside>
+
+            {isSidebarOpen && (
+                <aside className="gap-4 w-64 bg-green-600 text-white flex flex-col p-4 h-screen fixed top-0 left-0 z-40">
+                    <h2 className="text-2xl font-bold mb-6 mt-12 text-center">{sidebarTitle}</h2>
+
+                    <nav className="flex-1">
+                        <ul className="space-y-4">
+                            <li>
+                                <Link to={dashboardLink} className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                    <Home size={25} />
+                                    <span>Dashboard</span>
+                                </Link>
+                            </li>
+
+                            {role === "MANAGER" ? (
+                                <li>
+                                    <Link to="/manager/inventory" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                        <Package size={25} />
+                                        <span>Inventory</span>
+                                    </Link>
+                                </li>
+                            ) : (
+                                <li>
+                                    <Link to="/admin/displayproducts" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                        <NotebookText size={25} />
+                                        <span>Display Products</span>
+                                    </Link>
+                                </li>
+                            )}
+
+                            {role === "MANAGER" ? (
+                                <>
+                                    <li>
+                                        <Link to="/manager/discounts"
+                                              className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                            <MdPercent size={25}/>
+                                            <span>Discounts</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/manager/customer-order"
+                                              className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                            <ClipboardList size={25}/>
+                                            <span>Customer Orders</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/manager/customer-feedback"
+                                              className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                            <MessageCircle size={25}/>
+                                            <span>Customer Feedback</span>
+                                        </Link>
+                                    </li>
+                                </>
+                            ) : (
+                                role === "ADMIN" && (
+                                    <>
+                                        <li>
+                                            <Link to="/admin/addproducts"
+                                                  className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                                <Plus size={20} />
+                                                <span>Add Products</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/admin/updateproducts" className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg">
+                                                <Pen size={20} />
+                                                <span>Update Products</span>
+                                            </Link>
+                                        </li>
+                                    </>
+                                )
+                            )}
+                        </ul>
+                    </nav>
+                    <div className="mt-auto mb-20 flex justify-center">
+                        <button
+                            onClick={handleLogout}
+                            className="w-10 h-10 bg-red-600 rounded-full flex items-center justify-center hover:bg-red-700 transition-transform duration-200 hover:scale-125">
+                            <LogOut size={24} className="text-white" />
+                        </button>
+                    </div>
+                </aside>
+            )}
+        </>
     );
+
 };
 
 export default Sidebar;
