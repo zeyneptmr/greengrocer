2a29e024e11dca9353424f46b0af556a46e69ad8
zeynepkrtls <zeyzeykurtulus@gmail.com>
2025-04-08 21:49:34 +0300

    navbar made recursive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -133,137 +133,103 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="h-24 w-full bg-white text-green-600 flex items-center px-4 relative">
-                <div className="h-full flex items-center">
-                    <img src={logo} alt="Tap-Taze Logo" className="h-full w-auto"/>
-                    <Link to="">
-                        <h1 className="text-6xl font-bold text-green-600 ml-3">TapTaze</h1>
-                    </Link>
+            <nav className="w-full bg-white text-green-600 px-4 relative flex flex-col md:flex-row items-center justify-between h-auto md:h-24 space-y-2 md:space-y-0">
+                <div className="flex items-center w-full md:w-auto justify-between">
+                    <div className="flex items-center">
+                        <img src={logo} alt="Tap-Taze Logo" className="h-16 md:h-20 w-auto" />
+                        <Link to="">
+                            <h1 className="text-3xl md:text-6xl font-bold text-green-600 ml-2">TapTaze</h1>
+                        </Link>
+                    </div>
                 </div>
 
-                {/* <SearchBar products={products} /> */}
-                <SearchBar />
+                <div className="w-full md:w-1/2 lg:w-1/3 mt-2 md:mt-0">
+                    <SearchBar />
+                </div>
 
-                <div className="flex items-center gap-10 ml-auto"> {/* Push the buttons to the right */}
+                <div className="flex items-center gap-4 md:gap-10 w-full md:w-auto justify-center md:justify-end">
                     {loggedInUser ? (
                         <div className="relative" ref={profileMenuRef}>
                             <button
                                 onClick={handleProfileMenuToggle}
-                                className="flex flex-col items-center bg-transparent text-green-600 p-1 rounded transition-transform hover:scale-110">
-                                <User size={44}/>
-                                <span className="text-xs">Profile</span> {/* Profile olarak değiştirildi */}
+                                className="flex flex-col items-center p-1 rounded transition-transform hover:scale-110"
+                            >
+                                <User size={32} />
+                                <span className="text-xs">Profile</span>
                             </button>
                             {isProfileMenuOpen && (
-                                <div
-                                    className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md shadow-md w-48 z-50">
+                                <div className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md shadow-md w-48 z-50">
                                     <ul>
-                                        <li
-                                            onClick={() => { navigate("/account"); setIsProfileMenuOpen(false); }}
-                                            className="p-2 cursor-pointer hover:bg-gray-200"
-                                        >
-                                            Account settings
-                                        </li>
-                                        <li
-                                            onClick={() => { navigate("/address"); setIsProfileMenuOpen(false); }}
-                                            className="p-2 cursor-pointer hover:bg-gray-200"
-                                        >
-                                            Addresses
-                                        </li>
-                                        <li
-                                            onClick={() => { handleLogout(); setIsProfileMenuOpen(false); }}
-                                            className="p-2 cursor-pointer hover:bg-gray-200"
-                                        >
-                                            Log Out
-                                        </li>
+                                        <li onClick={() => { navigate("/account"); setIsProfileMenuOpen(false); }} className="p-2 cursor-pointer hover:bg-gray-200">Account settings</li>
+                                        <li onClick={() => { navigate("/address"); setIsProfileMenuOpen(false); }} className="p-2 cursor-pointer hover:bg-gray-200">Addresses</li>
+                                        <li onClick={() => { handleLogout(); setIsProfileMenuOpen(false); }} className="p-2 cursor-pointer hover:bg-gray-200">Log Out</li>
                                     </ul>
                                 </div>
                             )}
                         </div>
                     ) : (
-                        <button onClick={() => setIsAccountOpen(true)}
-                                className="flex flex-col items-center bg-transparent text-green-600 p-1 rounded transition-transform hover:scale-110">
-                            <User size={44}/>
+                        <button onClick={() => setIsAccountOpen(true)} className="flex flex-col items-center p-1 rounded transition-transform hover:scale-110">
+                            <User size={32} />
                             <span className="text-xs">Log In</span>
                         </button>
                     )}
 
                     <Link to="/favorites">
-                        <button
-                            className="flex flex-col items-center bg-transparent text-green-600 p-1 rounded transition-transform hover:scale-110 relative">
-                            <Heart size={44}/>
+                        <button className="flex flex-col items-center p-1 rounded transition-transform hover:scale-110 relative">
+                            <Heart size={32} />
                             <span className="text-xs">Favorites</span>
                             {favorites.length > 0 && (
-                                <span
-                                    className="absolute top-[-5px] right-[5px] bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-                                    {favorites.length}
-                                </span>
+                                <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                  {favorites.length}
+                </span>
                             )}
                         </button>
                     </Link>
 
-                    <button
-                        onClick={handleCartClick}
-                        className="flex flex-col items-center bg-transparent text-green-600 p-1 rounded transition-transform hover:scale-110 relative">
-                        <ShoppingCart size={44}/>
-                        <span
-                            className="text-xs">{loggedInUser ? "My Cart" : "Cart"}</span> {/* Cart butonunun yazısı dinamik olarak değiştirildi */}
+                    <button onClick={handleCartClick} className="flex flex-col items-center p-1 rounded transition-transform hover:scale-110 relative">
+                        <ShoppingCart size={32} />
+                        <span className="text-xs">{loggedInUser ? "My Cart" : "Cart"}</span>
                         {getTotalProductTypes() > 0 && (
-                            <span
-                                className="absolute top-[-5px] right-[-5px] bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-                                {getTotalProductTypes()}
-                            </span>
+                            <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                {getTotalProductTypes()}
+              </span>
                         )}
                     </button>
                 </div>
             </nav>
 
             {isCartAccessRestricted && (
-                <div className="absolute top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
+                <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
                     <div className="relative bg-white p-6 rounded-lg shadow-lg text-center">
-                        {/* Çarpı Butonu (Popup'ın Üzerinde, Sol Üst Köşede) */}
-                        <button
-                            onClick={() => setIsCartAccessRestricted(false)}
-                            className="absolute -top-4 left-0 bg-red-600 text-white rounded-full w-8 h-8 flex items-center justify-center text-lg hover:bg-red-800"
-                        >
+                        <button onClick={() => setIsCartAccessRestricted(false)} className="absolute -top-4 left-0 bg-red-600 text-white rounded-full w-8 h-8 flex items-center justify-center text-lg hover:bg-red-800">
                             ✖
                         </button>
-
-                        <p className="text-lg font-semibold mb-4">Please log in to continue !</p>
-                        <button
-                            onClick={() => {
-                                setIsAccountOpen(true);
-                                setIsCartAccessRestricted(false);
-                            }}
-                            className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700"
-                        >
+                        <p className="text-lg font-semibold mb-4">Please log in to continue!</p>
+                        <button onClick={() => { setIsAccountOpen(true); setIsCartAccessRestricted(false); }} className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
                             Log In
                         </button>
                     </div>
                 </div>
             )}
 
-            <div className="bg-green-500 text-white p-3 relative">
-                <div className="flex justify-center">
-                    <ul className="flex space-x-6 relative">
-                        <li className="cursor-pointer transform transition-all duration-300 hover:scale-125 hover:text-orange-500 flex items-center justify-center">
+            <div className="bg-green-500 text-white p-3">
+                <div className="flex justify-center overflow-x-auto">
+                    <ul className="flex space-x-4 sm:space-x-6">
+                        <li className="cursor-pointer hover:scale-125 hover:text-orange-500">
                             <Link to="">
-                                <Home size={25} className="inline-block mr-1"/>
+                                <Home size={25} />
                             </Link>
                         </li>
                         {menuItems.map((menu, index) => (
-                            <li
-                                key={index}
-                                className="relative cursor-pointer transform transition-all duration-300 hover:scale-110 hover:text-orange-500 z-20 p-1 rounded-md"
-                                onClick={() => handleMenuClick(menu.name)}
-                            >
-                                <span className="flex items-center text-xl">{menu.name}</span>
+                            <li key={index} onClick={() => handleMenuClick(menu.name)} className="cursor-pointer hover:scale-110 hover:text-orange-500">
+                                <span className="text-sm sm:text-lg">{menu.name}</span>
                             </li>
                         ))}
                     </ul>
                 </div>
             </div>
 
-            <Account isOpen={isAccountOpen} onClose={() => setIsAccountOpen(false)}/>
+            <Account isOpen={isAccountOpen} onClose={() => setIsAccountOpen(false)} />
         </>
     );
 };
