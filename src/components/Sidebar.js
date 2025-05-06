13972b9e1ff7109255a930c8358d753f2fddd872
zeyneptmr <zeyneptemur02@icloud.com>
2025-05-06 04:04:08 +0300

    admin&manager sidebar have language support now

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { Package, Box, Home, Users, LogOut, MessageCircle } from "lucide-react";
 import { Plus, Pen, NotebookText, Truck, ClipboardList } from 'lucide-react';
@@ -6,6 +6,9 @@ import { MdPercent } from 'react-icons/md';
 import axios from "axios";
 import { useCart } from "../helpers/CartContext";
 import { useFavorites } from "../helpers/FavoritesContext";
+import { LanguageContext } from '../context/LanguageContext';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
 
 const Sidebar = () => {
     const [loggedInUser, setLoggedInUser] = useState(null);
@@ -13,6 +16,20 @@ const Sidebar = () => {
     const [role, setRole] = useState("");
     const {setIsLoggedIn} = useCart();
     const {refreshAuth} = useFavorites();
+    const { language, setLanguage } = useContext(LanguageContext);
+    const [isLanguageMenuOpen, setIsLanguageMenuOpen] = useState(false);
+    const { t } = useTranslation('sidebar');
+
+
+    const handleLanguageChange = (lang) => {
+        setLanguage(lang);
+        i18n.changeLanguage(lang);
+        setIsLanguageMenuOpen(false);
+    };
+
+    const toggleLanguageMenu = () => {
+        setIsLanguageMenuOpen(!isLanguageMenuOpen);
+    };
 
 
     useEffect(() => {
@@ -46,7 +63,7 @@ const Sidebar = () => {
             });
     };
 
-    const sidebarTitle = role === "MANAGER" ? "Manager Panel" : role === "ADMIN" ? "Admin Panel" : "User Home";
+    const sidebarTitle = role === "MANAGER" ? t("manager_panel") : role === "ADMIN" ? t("admin_panel") : t("user_home");
     const dashboardLink = role === "MANAGER" ? "/manager" : role === "ADMIN" ? "/admin" : "/user/home";
 
 
@@ -62,7 +79,7 @@ const Sidebar = () => {
                                 className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                             >
                                 <Home size={25}/>
-                                <span>Dashboard</span>
+                                <span>{t("dashboard")}</span>
                             </Link>
                         </li>
 
@@ -73,7 +90,7 @@ const Sidebar = () => {
                                     className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                 >
                                     <Package size={25}/>
-                                    <span>Inventory</span>
+                                    <span>{t("inventory")}</span>
                                 </Link>
                             </li>
                         ) : (
@@ -83,7 +100,7 @@ const Sidebar = () => {
                                     className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                 >
                                     <NotebookText size={25}/>
-                                    <span>Display Products</span>
+                                    <span>{t("display_products")}</span>
                                 </Link>
                             </li>
                         )}
@@ -96,7 +113,7 @@ const Sidebar = () => {
                                         className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                     >
                                         <MdPercent size={25}/>
-                                        <span>Discounts</span>
+                                        <span>{t("discounts")}</span>
                                     </Link>
                                 </li>
                                 <li>
@@ -105,7 +122,7 @@ const Sidebar = () => {
                                         className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                     >
                                         <ClipboardList size={25}/>
-                                        <span>Customer Orders</span>
+                                        <span>{t("customer_orders")}</span>
                                     </Link>
                                 </li>
                                 <li>
@@ -114,7 +131,7 @@ const Sidebar = () => {
                                         className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                     >
                                         <MessageCircle size={25}/>
-                                        <span>Customer Forms</span>
+                                        <span>{t("customer_forms")}</span>
                                     </Link>
                                 </li>
                             </>
@@ -127,7 +144,7 @@ const Sidebar = () => {
                                             className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                         >
                                             <Plus size={20}/>
-                                            <span>Add Products</span>
+                                            <span>{t("add_products")}</span>
                                         </Link>
                                     </li>
                                     <li>
@@ -136,7 +153,7 @@ const Sidebar = () => {
                                             className="flex items-center space-x-3 hover:bg-green-700 p-3 rounded-lg"
                                         >
                                             <Pen size={20}/>
-                                            <span>Update Products</span>
+                                            <span>{t("update_products")}</span>
                                         </Link>
                                     </li>
                                 </>
@@ -144,6 +161,38 @@ const Sidebar = () => {
                         )}
                     </ul>
                 </nav>
+
+                {/* Dil seçme butonu */ }
+                <div className="relative flex justify-center mb-4">
+                    <button
+                        onClick={toggleLanguageMenu}
+                        className="flex flex-col items-center gap-1 px-2 py-1 rounded transition-transform hover:scale-110 relative"
+                    >
+                        <span className="text-3xl">🌍</span>
+                        <span className="text-sm">{language.toUpperCase()}</span>
+                    </button>
+                    {isLanguageMenuOpen && (
+                        <div className="absolute top-14 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md shadow-md w-32 z-50 text-black">
+                            <ul>
+                                <li onClick={() => handleLanguageChange('en')}
+                                    className="p-2 cursor-pointer hover:bg-gray-200 text-lg">🇬🇧 English
+                                </li>
+                                <li onClick={() => handleLanguageChange('tr')}
+                                    className="p-2 cursor-pointer hover:bg-gray-200 text-lg">🇹🇷 Türkçe
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
+
+
+
+
+
+
+
+
                 <div className="mt-auto mb-20 flex justify-center">
                     <button
                         onClick={handleLogout}
